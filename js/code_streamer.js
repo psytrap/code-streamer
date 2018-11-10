11646f1ebeee496c83b05addc2d7c552aae43e2b
mik <do-not-reply@mik.net>
2018-11-10 11:05:59 +0100

    Remove dead code

--- js/code_streamer.js
+++ js/code_streamer.js
@@ -1,30 +1,15 @@
 
-// create pdf of 10x15cm
-
-// create and read code
-// load cover
-// generate PDF
-
-
-function Test_jspdf_Create()
+///
+///
+function CreatePdf()
 {
-	console.log("Test_jspdf_Create");
-	// get url // https://i.imgur.com/ZOuqd4f.jpg // CORS bad // http://www.brooklynvegan.com/files/2018/08/aphex-twin-collapse.jpg
+	console.log("CreatePdf");
 	var file_data = document.getElementById("cover").files[0];
 	var url = document.getElementById("url").value;
 	console.log(file_data);
-
-// CORS bad	
-//toDataURL(url, function(dataURL){
-
-// TODO JFIF -> 4AAQSkZJRgABAQAAAQABAAD
-
-
-//		doc.save('Test_jspdf_Create.pdf');
-// console.log("KOTZ");
 	var url_a = document.createElement('a');
 	url_a.setAttribute('href', url);
-	var hostname = url_a.hostname;  //  'example.com'
+	var hostname = url_a.hostname;
 
 	if(hostname === "open.spotify.com")
 	{
@@ -37,18 +22,10 @@ function Test_jspdf_Create()
 		qrcode.makeCode(url);
 		setTimeout(function(){ fetchQrCode(qr_div, file_data); }, 100);
 	}
-
-//	
-//});
-// 'https://upload.wikimedia.org/wikipedia/commons/7/75/Information-silk.png'
-//	kotz(url)
-
 }
 
-
-
-
-
+///
+///
 function readFile(file_data, url_data, spotify) {
 	console.log("file " + file_data)
 
@@ -88,19 +65,14 @@ function readFile(file_data, url_data, spotify) {
 	reader.readAsDataURL(file_data);
 }
 
-
-
+///
+///
 function GetImageSize(image_data, callback)
 {
 	var img = new Image(); // document.createElement('img');
 
 	img.onload = function() 
 	{
-		// HATE
-		// JS
-		// no sync function for nanosecond operation!!!!!!!!!!!
-		// HATE
-		// JS
 		var width = img.naturalWidth;
 		var height = img.naturalHeight;
 		callback(width, height);
@@ -109,6 +81,8 @@ function GetImageSize(image_data, callback)
 	img.src = image_data;
 }
 
+///
+///
 function fetchQrCode(qr_div, file_data)
 {
 	var qr_img = qr_div.lastElementChild;
@@ -117,6 +91,8 @@ function fetchQrCode(qr_div, file_data)
 	readFile(file_data, url_data);
 }
 
+///
+///
 function fetchSpotify(url, file_data)
 {
 	var url_a = document.createElement('a');
@@ -141,7 +117,6 @@ function fetchSpotify(url, file_data)
 	var spoty_url = "https://scannables.scdn.co/uri/plain/png/ffffff/black/1000/" + spoty_codes.join(':');
 	console.log("Spotify: " + spoty_url);
 	var xhr = new XMLHttpRequest();
-//    xhr.header("Access-Control-Allow-Origin", "*");
 	xhr.open('get', spoty_url);
 	xhr.responseType = 'blob';
 	xhr.onload = function()
@@ -166,137 +141,5 @@ function fetchSpotify(url, file_data)
 		img.src = URL.createObjectURL(xhr.response);
 	};
 	xhr.send();
-
 }
 
-
-
-function dead(dead)
-{
-	var xhr = new XMLHttpRequest();
-//    xhr.header("Access-Control-Allow-Origin", "*");
-	xhr.open('get', spoty_url);
-	xhr.responseType = 'blob';
-	xhr.onload = function()
-	{
-
-		fr = new FileReader();
-		fr.onload = function(event)
-		{
-			console.log(fr.result);
-			readFile(file_data, fr.result, true);
-		}
-		fr.readAsDataURL(xhr.response)
-	};
-	xhr.send();
-	var img = new Image();
-	img.onload = function()
-	{
-	};
-	img.src = spoty_url;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/// FRIEDHOF
-
-
-
-
-
-
-
-
-
-
-function fetchUrlQrCode(url, file_data, url_callback)
-{
-	var qr_url_rest= "https://api.qrserver.com/v1/create-qr-code/?data=" + url + "&size=500x500&margin=50"
-	var xhr = new XMLHttpRequest();
-//    xhr.header("Access-Control-Allow-Origin", "*");
-	xhr.open('get', qr_url_rest);
-	xhr.responseType = 'blob';
-	xhr.onload = function()
-	{
-		console.log(xhr.response);
-		url_callback(file_data, xhr.response);
-	};
-	xhr.send();
-}
-
-function toDataURL(url, callback){
-    var xhr = new XMLHttpRequest();
-//    xhr.header("Access-Control-Allow-Origin", "*");
-    xhr.open('get', url);
-    xhr.responseType = 'blob';
-    xhr.onload = function(){
-      var fr = new FileReader();
-    
-      fr.onload = function(){
-        callback(this.result);
-      };
-    
-      fr.readAsDataURL(xhr.response); // async call
-    };
-    
-    xhr.send();
-}
-
-function kotz(url)
-{
-	console.log("aaaaaaaa " + url)
-	self.addEventListener('fetch', function(event)
-	{
-		event.respondWith(
-			fetch(url, {mode: 'no-cors'})
-			  	.then(function(response) {console.log("sssss " +url + response.status) }) //fetch_response)
-				.catch(function(err)
-				{
-					console.log('Fetch Error :-S', err);
-				})
-			)
-	})
-}
-
-function fetch_response(response)
-{
-	console.log("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
-	if (response.status !== 200)
-	{
-		console.log('Looks like there was a problem. Status Code: ' +
-		response.status);
-	        // return;
-	}
-
-	// Examine the text in the response
-	response.blob().then(function(data)
-	{
-		console.log(data);
-	})
-	var fr = new FileReader();
-	fr.onload = function(data_url)
-	{
-		console.log(data_url.target.result);
-	}
-	fr.readAsDataURL(data); // async call
-
-}
